3ea1b5f777831d2d892aafc96f3865878a660d13
MuhammadAlwiNugraha <62659068+MuhammadAlwiNugraha@users.noreply.github.com>
2021-08-19 11:45:31 +0700

    Update faqs.php

--- application/views/home/faqs.php
+++ application/views/home/faqs.php
@@ -23,32 +23,31 @@
                     <div class="card-header" id="headingOne">
                         <h2 class="mb-0">
                             <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                Apa perbedaan LPK KINJADI dengan LPK yang lainnya?
+                                Apa itu LPK Kinjadi
                             </button>
                         </h2>
                     </div>
 
                     <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
                         <div class="card-body">
-                            Siapa saja yang dapat mengikuti program di LPK KINJADI? Peserta mulai dari Lulusan SMA sampai lulusan Sarjana/S1 dapat
-                            mengikuti program-program yang telah tersedia di LPK KINJADI. Program-programnya dapat kamu lihat <a target="_blank" href="http://api.whatsapp.com/send?phone=6281222137799&text=Hallo%20Kak,%20Pengen%20tau%20lebih%20lanjut%20tentang%20program%20magang%20di%20LPK%20Kinjadi">
-                                disini!</a>
-                            Bagaimana LPK KINJADI meningkatkan pembelajaran kepada para Peserta? LPK KINJADI selalu berusaha meningkatkan mutu
-                            pembelajaran baik dari fasilitas maupun cara belajar demi kenyamanan selama pembelajaran di LPK KINJADI dan menerapkan
-                            mengenai sikap etos kerja sesuai dengan budaya Jepang. </div>
+                            LPK Kinjadi merupakan Lembaga Pelatihan dan Keterampilan yang memiliki program magang ke Jepang, yaitu program yang di peruntukkan bagi
+                            pemuda pemudi Indonesia untuk melakukan magang di perusahaan penerima di Jepang dalam kurun waktu tertentu.
+                        </div>
                     </div>
                 </div>
                 <div class="card">
                     <div class="card-header" id="headingTwo">
                         <h2 class="mb-0">
                             <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                Collapsible Group Item #2
+                                Siapa saja yang dapat mengikuti program di LPK KINJADI?
                             </button>
                         </h2>
                     </div>
                     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
                         <div class="card-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            Peserta mulai dari Lulusan SMA sampai lulusan Sarjana/S1 dapat
+                            mengikuti program-program yang telah tersedia di LPK KINJADI. Program-programnya dapat kamu tanyakan <a target="_blank" href="http://api.whatsapp.com/send?phone=6281222137799&text=Hallo%20Kak,%20Pengen%20tau%20lebih%20lanjut%20tentang%20program%20magang%20di%20LPK%20Kinjadi">
+                                disini!</a>
                         </div>
                     </div>
                 </div>
@@ -56,13 +55,15 @@
                     <div class="card-header" id="headingThree">
                         <h2 class="mb-0">
                             <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                Collapsible Group Item #3
+                                Bagaimana LPK KINJADI meningkatkan pembelajaran kepada para Peserta?
                             </button>
                         </h2>
                     </div>
                     <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
                         <div class="card-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            LPK KINJADI selalu berusaha meningkatkan mutu
+                            pembelajaran baik dari fasilitas maupun cara belajar demi kenyamanan selama pembelajaran di LPK KINJADI dan menerapkan
+                            mengenai sikap etos kerja sesuai dengan budaya Jepang.
                         </div>
                     </div>
                 </div>
@@ -83,38 +84,26 @@
 
                     <div id="collapseOne1" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
                         <div class="card-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            Daftarkan melalui kontak sosial media yang tersedia
                         </div>
                     </div>
                 </div>
                 <div class="card">
-                    <div class="card-header" id="headingTwo">
+                    <div class="card-header" id="headingOne">
                         <h2 class="mb-0">
-                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo1" aria-expanded="false" aria-controls="collapseTwo">
-                                Collapsible Group Item #2
+                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne1" aria-expanded="false" aria-controls="collapseOne">
+                                Program apa saja yang ada di LPK KINJADI
                             </button>
                         </h2>
                     </div>
-                    <div id="collapseTwo1" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+
+                    <div id="collapseOne1" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
                         <div class="card-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            Terdapat tiga program utama yang ada di LPK KINJADI yaitu JISSHUSEI, NIHONGO GAKKOU, TOKUTEI GINOU.
                         </div>
                     </div>
                 </div>
-                <div class="card">
-                    <div class="card-header" id="headingThree">
-                        <h2 class="mb-0">
-                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree1" aria-expanded="false" aria-controls="collapseThree">
-                                Collapsible Group Item #3
-                            </button>
-                        </h2>
-                    </div>
-                    <div id="collapseThree1" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-                        <div class="card-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                        </div>
-                    </div>
-                </div>
+
             </div>
         </div>
     </div>
