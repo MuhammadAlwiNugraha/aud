01c1f54196a262b2d5974178a59411db8f597e6f
MuhammadAlwiNugraha <62659068+MuhammadAlwiNugraha@users.noreply.github.com>
2021-08-20 13:54:25 +0700

    Update home.php

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -5,7 +5,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="shortcut icon" href="<?= base_url('assets/') ?>img/favicon.ico" type="image/ico">
+
 	<title>LPK KINJADI</title>
 </head>
 
